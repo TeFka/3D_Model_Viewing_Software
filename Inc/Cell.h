8e2a228373600440bd5fc8a9f25c6687b8e0371a
michael michael <michaeljohnmichael2001@gmail.com>
2020-12-05 19:49:04 +0000

    cell.h

--- Inc/Cell.h
+++ Inc/Cell.h
@@ -1,101 +1,51 @@
+#include <iostream>
+#include <vector>
+
+#ifndef CELL_H
+#define CELL_H
 //------------------------------------------------------------------
-class cell
+class Cell
 {
 protected:
     colour col;
     int ID;
-    std::vector<vertice> cellvertices
-
-    ...centre of gracity
-    material ID
-    weight
-    density
-
+    int materialID;
+    float weight;
+    float density;
+    float volume;
+    std::vector<int> indices;
+    vector centre_of_gravity;
 public:
     virtual double getvolume();
     void setcolour( colour c);
 };
 //------------------------------------------------------------------
-//defining functions of the cell class
-double cell::getvolume()
-{
-    return 0;//not actually calculating volume of cell here so only dummy function is needed
-}
-
-void cell::setcolour(colour c)
-{
-    this->col = c;//this sets the colour for all the cells
-}
-//------------------------------------------------------------------------
-class tetrahedron : public cell
+class Tetrahedron : public cell
 {
 private:
     vector3d p1, p2, p3, p4;
 public:
-    tetrahedron(vector3d p1, p2, p3, p4);
+    Tetrahedron(vector3d p1, p2, p3, p4);
     double getvolume();
 };
-//defining functions for the tetrahedron
-tetrahedron::tetrahedron(vector3d p1, p2, p3, p4)
-{
-    this->p1 = p1;
-    this->p2 = p2;
-    this->p3 = p3;
-    this->p4 = p4;
-}
-double tetrahedron::getvolume()//will overwrite original getvolume function in cell class
-{
-    //code to calculate volume of...
-}
 //---------------------------------------------------------------
-class pyramid : public cell
+class Pyramid : public cell
 {
 private:
     vector3d p1, p2, p3, p4, p5;
 public:
-    pyramid(vector3d p1, p2, p3, p4, p5);
+    Pyramid(vector3d p1, p2, p3, p4, p5);
     double getvolume();
 };
-//defining functions for pyramid
-pyramid::pyramid(vector3d p1, p2, p3, p4, p5)
-{
-    this->p1 = p1;
-    this->p2 = p2;
-    this->p3 = p3;
-    this->p4 = p4;
-    this->p5 = p5;
-}
-
-pyramid::getvolume()
-{
-//   ...
-}
 //---------------------------------------------------------------
-class hexahedron : public cell
+class Hexahedron : public cell
 {
 private:
     vector3d p1, p2, p3, p4, p5, p6, p7, p8;
 public:
-    hexahedron(vector3d p1, p2, p3, p4, p5, p6, p7, p8);
+    Hexahedron(vector3d p1, p2, p3, p4, p5, p6, p7, p8);
     double getvolume();
 };
-//defining functions for hexahedron
-hexahedron::hexahedron(vector3d p1, p2, p3, p4, p5, p6, p7, p8)
-{
-    this->p1 = p1;
-    this->p2 = p2;
-    this->p3 = p3;
-    this->p4 = p4;
-    this->p5 = p5;
-    this->p6 = p6;
-    this->p7 = p7;
-    this->p8 = p8;
-}
-
-hexahedron::getvolume()
-{
-    //...
-}
-
+#endif // CELL_H
 
 
